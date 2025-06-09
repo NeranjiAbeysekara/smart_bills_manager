8e60491a9e5a474caf01c6612412cd227a26b885
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2025-06-09 13:53:47 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js' 
+
+// initialize express app
+const app = express()
+
+// connect to MongoDB       
+await connectDB() 
+// middleware
+app.use(cors())
+app.use(express.json())     
+// routes
+app.get('/', (req, res) => 
+  res.send('Welcome to the backend server!'))
+//port
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
