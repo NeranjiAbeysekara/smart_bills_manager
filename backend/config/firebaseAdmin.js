3fcb44281e912f64a97a8159e6cfa393d582bd9e
Shadini Nirasha <146790732+ShadiniMalawige@users.noreply.github.com>
2025-08-24 23:43:12 +0530

    Update the firebaseAdmin.js

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,11 +1,14 @@
 // backend/config/firebaseAdmin.js
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable is missing");
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(
-    readFileSync('./firebase-service-account.json', 'utf8')
-  ))
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
